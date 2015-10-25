eb2d3d1233b00291e8b6f9fa31b1d816681222f1
Eklove <ekluv@live.com>
2015-10-25 19:38:01 +0530

    stream-api

---
+++ check_api.py
@@ -0,0 +1,54 @@
+import json
+from datetime import datetime,timedelta
+from dateutil.parser import parse
+import time
+
+local=datetime.now()
+
+tweets_data_path = 'data.txt'
+
+tweets_file = open(tweets_data_path, "r")
+
+class User(object):
+    name=""
+    urls=[]
+
+    def display(self):
+        print self.name
+        print self.link
+
+
+while(True):
+
+    users=[]
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            user=User()
+            created_at=parse(tweet["created_at"])
+            created_at=created_at.strftime("%Y-%m-%d %H:%M:%S")
+            created_at=parse(created_at)
+            created_at=created_at +timedelta(hours=5,minutes=30)
+            if((abs(local-created_at).total_seconds())/60 > 5):
+                local=datetime.now()
+
+            else:
+
+                user.name=tweet["user"]["name"]
+                for url in tweet["entities"]['urls']:
+                    user.urls.append( url['expanded_url'])
+                users.append(user)
+        except:
+            continue
+
+    for user in users:
+        print "User Name :" + user.name
+        for url in user.urls:
+            print url
+        print "Total Urls Related :%d" %(len(user.urls))
+        print "****************"
+
+    print "------------------------------------------------------------"
+    time.sleep(60)
+
+
---
+++ streaming_api.py
@@ -0,0 +1,31 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy import api
+import time
+
+access_token = "4010780236-Q2YCnYqSCgdmTMiT2G6Hj2YGxmH34pt3slVEueM"
+access_token_secret = "0DDNg2tS2XJK32bPggoJXzRX2W2xJVUxmFw1kdMLmA0J1"
+consumer_key = "Of9RURntChba6Ygrt5egqJe2E"
+consumer_secret = "0nm1zr65lkdpM6RKNPQEiGLa3Xq7B3v6jUDxoXwFKrlN7yqTc7"
+
+f = open("data.txt", "w")
+class Listener(StreamListener):
+
+    def on_data(self,data):
+        f.write(data+'\n')
+        print "writing object to file"
+        return True
+
+    def on_error(self, status):
+        print status
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+stream = Stream(auth, Listener())
+print "Enter a keyword"
+keyword=raw_input()
+stream.filter(track=[keyword])
+
+
