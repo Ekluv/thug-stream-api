8785cce11333a0ee3af57a2efc9f4b07218d497c
Ekluv <ekluv@live.com>
2015-12-13 15:39:02 +0530

    Update streaming_api.py

--- streaming_api.py
+++ streaming_api.py
@@ -2,10 +2,10 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-access_token = "4010780236-Q2YCnYqSCgdmTMiT2G6Hj2YGxmH34pt3slVEueM"
-access_token_secret = "0DDNg2tS2XJK32bPggoJXzRX2W2xJVUxmFw1kdMLmA0J1"
-consumer_key = "Of9RURntChba6Ygrt5egqJe2E"
-consumer_secret = "0nm1zr65lkdpM6RKNPQEiGLa3Xq7B3v6jUDxoXwFKrlN7yqTc7"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 f = open("data.txt", "w")
 class Listener(StreamListener):
